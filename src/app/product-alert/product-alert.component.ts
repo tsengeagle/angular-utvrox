2fdb2734e1a452a2a2ef581639280402e9f0b3ad
tsengeagle <tsengeagle@gmail.com>
2020-02-26 17:42:34 +0800

    Initial commit

---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import { Input } from "@angular/core";
+import { Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "app-product-alert",
+  templateUrl: "./product-alert.component.html",
+  styleUrls: ["./product-alert.component.css"]
+})
+export class ProductAlertComponent implements OnInit {
+  constructor() {}
+
+  @Output() notify=new EventEmitter();
+  @Input() product;
+  ngOnInit() {}
+}
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share(name: String) {
+    window.alert(name + ' has been shared!');
+  }
+  onNotify(){
+    window.alert('notify!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
